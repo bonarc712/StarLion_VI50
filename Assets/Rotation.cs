a1516cb95accb3f18f82b701d00de13c3aceb24a
Loïc Roberge <lorob64@gmail.com>
2015-11-26 15:08:38 +0100

    First commit, included project started last week. .gitignore is also included.

---
+++ Assets/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float xSpeedR = 10;
+	public float ySpeedR = 10;
+	public float zSpeedR = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,Input.GetAxis("Mouse X") * xSpeedR,0);
+	}
+}
---
+++ Assets/RotationControler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationControler : MonoBehaviour {
+
+	public float xSpeedR = 5;
+	public float ySpeedR = 5;
+	public float zSpeedR = 5;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (-Input.GetAxis ("Vertical") * xSpeedR, Input.GetAxis ("Horizontal") * ySpeedR,Input.GetAxis ("Horizontal") * zSpeedR );
+	
+	}
+}
