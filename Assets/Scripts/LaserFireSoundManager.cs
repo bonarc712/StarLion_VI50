d1f429b669dab95e7cd6d1f1581130d66800d3dc
Loïc Roberge <lorob64@gmail.com>
2015-12-26 15:28:13 +0100

    Added sound effects to laser fire, as well as the fact that it is triggered by the mouse

---
+++ Assets/Scripts/LaserFireSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserFireSoundManager : MonoBehaviour {
+	
+	public static AudioSource laserFire;
+	
+	// Use this for initialization
+	void Start () {
+		laserFire = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Le son LaserFire est un son gratuit obtenu via freesfx.co.uk
+	public static void PlaySound(string soundName){
+		switch (soundName) {
+		case "LaserFire" : laserFire.Play(); break;
+		default : break;
+		}
+	}
+}
---
+++ Assets/Scripts/SpaceshipFireBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipFireBehaviour : MonoBehaviour {
+
+	public ParticleSystem particle;
+
+	// Use this for initialization
+	void Start () {
+		particle = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			particle.Play();
+			LaserFireSoundManager.PlaySound("LaserFire");
+		}
+	}
+}
